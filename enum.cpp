69574aa0e8aa2b8605bb1f02160fb0f5e24ae93f
Ambar Arain <91412242+Ambar-Ghaffar@users.noreply.github.com>
2021-10-09 11:09:37 +0500

    Enumuration Contribution

---
+++ enum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+enum days_week{Mon, Tues, Wed, Thu, Fri, Sat, Sun};
+
+int main()
+{	
+	days_week day1=Mon;
+	days_week day2=Thu;
+	cout<<"day1="<<day1+1;
+	cout<<"\n day2="<<day2;
+	
+	cout<<"\n result="<<(day2>day1);
+	
+}
+
